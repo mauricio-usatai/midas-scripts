cfed5836a78200d7a15a95837d4a42bcd44da8a4
mauricio.usatai@gmail.com <mauricio.usatai@gmail.com>
2023-09-21 10:17:53 -0300

    Better time schedule 20:50

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ from utils import DotSpinner
 from dag import run_dag
 
 
-schedule.every().day.at("10:01").do(run_dag)
+schedule.every().day.at("20:50").do(run_dag)
 spinner = DotSpinner("Waiting")
 
 while True:
